315b4ccafb90adf4da546c652643624a996c8082
Brendan <58452454+BrendanORAS@users.noreply.github.com>
2020-08-20 22:09:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="./safertracerlogo.PNG" className="App-logo" alt="logo" />
+          <img src="src/safertracerlogo.PNG" className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload. If this appears, then editing has been successful.
           </p>
