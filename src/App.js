de326010803e28cc5757f529ff5240bd32d6f288
Brendan <58452454+BrendanORAS@users.noreply.github.com>
2020-08-22 16:34:12 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
-location.replace("https://safertracerfarrerpark.s3.amazonaws.com/index.html")
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator } from 'aws-amplify-react'
+import Amplify, { Auth } from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            location.replace("https://safertracerfarrerpark.s3.amazonaws.com/index.html")
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default withAuthenticator(App, true);
